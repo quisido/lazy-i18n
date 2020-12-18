c07dbe8ac4755e50a83850be164ca5f0956d5bd6
Charles Stover <chastove@amazon.com>
2020-12-18 03:13:48 -0800

    attempt to fix depth error

--- src/components/provider/provider.view.tsx
+++ src/components/provider/provider.view.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, memo } from 'react';
 import TranslateFunctionContext from '../../contexts/translate-function';
+import TranslationsProp from '../../types/translations-prop';
 import * as useProviderHook from './provider.hook';
 import Props from './types/props';
 
@@ -11,7 +12,14 @@ function I18nProvider({
   locale,
   ...translationsRecord
 }: Props): ReactElement {
-  const { value } = useProvider({ fallbackLocale, locale, translationsRecord });
+  const { value } = useProvider({
+    fallbackLocale,
+    locale,
+    translationsRecord: translationsRecord as Record<
+      string,
+      TranslationsProp | undefined
+    >,
+  });
 
   return (
     <TranslateFunctionContext.Provider value={value}>
